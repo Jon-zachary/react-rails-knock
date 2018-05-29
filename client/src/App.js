7daa9f90c5989ef2c181a4c52c190a356a01ce73
Jon Zachary <jontzachary@gmail.com>
2018-05-29 09:57:45 -0400

    add basic client view

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,31 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+const BASE_URL = process.env.REACT_APP_BASE_URL;
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      juices: [],
+    };
+  }
+
+  componentDidMount() {
+    fetch(`${BASE_URL}/juices`)
+    .then(res => res.json())
+    .then(data => this.setState({
+      juices: data,
+    })).catch(err => console.log(err));
+
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        HENDUE
+        {this.state.juices.map(juice => {
+          return <p key={juice.id}> Name:{juice.name}, Sugar:{juice.sugar} </p>
+        })}
       </div>
     );
   }
